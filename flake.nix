680ef397ea3782d62fc3796dc9531b5598ce1449
Afillatedcarbon <71583904+Afillated@users.noreply.github.com>
2025-07-16 20:53:44 +0530

    intial commit 2

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "hauntedcupoftea's hauntedcupofdotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nh.url = "github:nix-community/nh";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+    };
+  };
+
+  outputs = {
+    self,
+    nh,
+    nixpkgs,
+    zen-browser,
+    ...
+  } @ inputs: {
+    nixosConfigurations = {
+      "hdabrosPC" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hdabrosPC
+        ];
+      };
+    };
+  };
+}
---
+++ hdabrosPC/default.nix
@@ -0,0 +1,215 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      
+    ];
+
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    # Enable GRUB
+    loader.grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      enableCryptodisk = false;
+      useOSProber = true; # Detect Windows
+      default = "saved"; # Default to last booted OS
+      configurationLimit = 8;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader.efi.canTouchEfiVariables = true;
+    plymouth.enable = true;
+
+    # Add NTFS support
+    supportedFilesystems = [ "ntfs" ];
+  };
+
+  networking.hostName = "carbon"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+  
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.carbon = {
+    isNormalUser = true;
+    description = "Afillatedcarbon";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = false;
+      gamescopeSession.enable = true;
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
+      protontricks.enable = true;
+    };
+
+    gamemode = {
+      enable = true;
+      settings.general.inhibit_screensaver = 0;
+    };
+
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+      args = [
+        "--backend sdl"
+        # "--rt"
+      ];
+    };
+  };
+
+
+  # Allow unfree 
+  nixpkgs.config.allowUnfree = true;
+
+ nix.settings.experimental-features = ["nix-command" "flakes"];
+  programs.nh = {
+    enable = true;
+    flake = "/home/tea/hauntedcupofdotfiles";
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep 8";
+    };
+  };
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   helix  
+   wget
+   vesktop
+   spotify
+   git
+   inputs.zen-browser.packages.${pkgs.system}.twilight
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
